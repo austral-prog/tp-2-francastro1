688a36b5664acde4e1b48cacc40b6433c023275f
francastro1 <162723525+francastro1@users.noreply.github.com>
2024-03-22 13:03:42 -0300

    change.py

--- change.py
+++ change.py
@@ -1,3 +1,7 @@
 def change():
     expense = 23.75
     money = 100
+    pesos = 76
+    centavos = 25
+    calculo=f"Ingresar gasto: \n{expense} \nDinero recibido \n{money} \n\nVuelto \n\nPesos:\n{pesos}\nCentavos:\n{centavos}"
+    print(calculo)
