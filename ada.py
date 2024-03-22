03c2a7998fb17f56fd4686b4f747d8e79313c825
francastro1 <162723525+francastro1@users.noreply.github.com>
2024-03-22 13:02:52 -0300

    ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,6 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    name=f"{first_name} {last_name}"
+    frase=f"{name.lower()} \n{name.title()} \n{name.upper()} \n\t {name.lower()}"
+    print(frase)
